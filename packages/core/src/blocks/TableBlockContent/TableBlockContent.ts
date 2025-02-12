e1bd16ceb7fd02d26ee8c1345b8abc51d25256a9
Nick the Sick <nick@blocknotejs.org>
2025-02-12 14:42:41 +0100

    feat: got the initial types working

--- packages/core/src/blocks/TableBlockContent/TableBlockContent.ts
+++ packages/core/src/blocks/TableBlockContent/TableBlockContent.ts
@@ -1,5 +1,4 @@
-import { Node } from "@tiptap/core";
-import { TableCell } from "@tiptap/extension-table-cell";
+import { TableCell as TiptapTableCell } from "@tiptap/extension-table-cell";
 import { TableHeader } from "@tiptap/extension-table-header";
 import { TableRow } from "@tiptap/extension-table-row";
 import { Node as PMNode } from "prosemirror-model";
@@ -13,10 +12,18 @@ import { mergeCSSClasses } from "../../util/browser.js";
 import { createDefaultBlockDOMOutputSpec } from "../defaultBlockHelpers.js";
 import { defaultProps } from "../defaultProps.js";
 import { EMPTY_CELL_WIDTH, TableExtension } from "./TableExtension.js";
+import { PropSchema } from "../../schema/index.js";
+import { NodeView } from "prosemirror-view";
 
 export const tablePropSchema = {
   textColor: defaultProps.textColor,
 };
+
+export const tableCellPropSchema = {
+  ...defaultProps,
+  colspan: { default: 1 },
+  rowspan: { default: 1 },
+} satisfies PropSchema;
 
 export const TableBlockContent = createStronglyTypedTiptapNode({
   name: "table",
@@ -101,12 +108,12 @@ export const TableBlockContent = createStronglyTypedTiptapNode({
       return new BlockNoteTableView(node, EMPTY_CELL_WIDTH, {
         ...(this.options.domAttributes?.blockContent || {}),
         ...HTMLAttributes,
-      });
+      }) as NodeView;
     };
   },
 });
 
-const TableParagraph = Node.create({
+const TableParagraph = createStronglyTypedTiptapNode({
   name: "tableParagraph",
   group: "tableContent",
   content: "inline*",
@@ -152,16 +159,20 @@ const TableParagraph = Node.create({
   },
 });
 
+export const TableCell = createBlockSpecFromStronglyTypedTiptapNode(
+  TableParagraph,
+  tableCellPropSchema
+);
+
 export const Table = createBlockSpecFromStronglyTypedTiptapNode(
   TableBlockContent,
   tablePropSchema,
   [
     TableExtension,
-    TableParagraph,
     TableHeader.extend({
       content: "tableContent",
     }),
-    TableCell.extend({
+    TiptapTableCell.extend({
       content: "tableContent",
     }),
     TableRow,
